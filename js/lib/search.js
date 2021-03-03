0600b025637410877dd3eb8c67c6daf73eba7ccb
offetuoso <offetuoso@gmail.com>
2021-03-03 16:25:42 +0900

    fuse.js 수정

--- js/lib/search.js
+++ js/lib/search.js
@@ -1,22 +1,27 @@
-summaryInclude=60;
 var fuseOptions = {
+  includeScore: true,
+  useExtendedSearch: true,
   shouldSort: true,
   includeMatches: true,
-  threshold: 0.0,
   tokenize:true,
+  /*threshold: 0.0,
   location: 0,
-  distance: 100,
+  distance: 100,*/
   maxPatternLength: 32,
   minMatchCharLength: 1,
   keys: [
     {name:"title",weight:0.8},
-    {name:"contents",weight:0.5},
+    {name:"contents",weight:0.1},
+    {name:"content",weight:0.1},
     {name:"tags",weight:0.3},
-    {name:"categories",weight:0.3}
+    {name:"description",weight:0.3}
   ]
-};
+}
 
-var searchQuery = param("s");
+
+var summaryInclude=60;
+var searchQuery = ''; //searchQuery = param("s");
+
 if(searchQuery){
   $("#search-query").val(searchQuery);
   executeSearch(searchQuery);
@@ -25,53 +30,63 @@ if(searchQuery){
 }
 
 function executeSearch(searchQuery){
+  var result;
   $.getJSON( "/index.json", function( data ) {
     var pages = data;
+    //console.log({"data":pages});
     var fuse = new Fuse(pages, fuseOptions);
-    var result = fuse.search(searchQuery);
-    console.log({"matches":result});
+    result = fuse.search(searchQuery);
+    //console.log({"matches":result});
+    
+    $("#local-search-result").empty();
+    
     if(result.length > 0){
       populateResults(result);
     }else{
-      $('#search-results').append("<p>No matches found</p>");
+   	 $('#local-search-result').append("<p>No matches found</p>");
     }
+    
   });
+  
+  return result;
 }
 
 
 function populateResults(result){
   $.each(result,function(key,value){
-  
-  console.log(value)
-  console.log(value.item)
-  
-    var contents= value.item.content;
+    var content= value.item.content;
     var snippet = "";
     var snippetHighlights=[];
     var tags =[];
     if( fuseOptions.tokenize ){
       snippetHighlights.push(searchQuery);
     }else{
-      $.each(value.matches,function(matchKey,mvalue){
-        if(mvalue.key == "tags" || mvalue.key == "categories" ){
+      $.each(value.matches,function(matchKey, mvalue){
+      
+        if(mvalue.key == "tags" || mvalue.key == "description" ){
           snippetHighlights.push(mvalue.value);
-        }else if(mvalue.key == "contents"){
+        }else if(mvalue.key == "content" || mvalue.key == "contents"){
+        
           start = mvalue.indices[0][0]-summaryInclude>0?mvalue.indices[0][0]-summaryInclude:0;
-          end = mvalue.indices[0][1]+summaryInclude<contents.length?mvalue.indices[0][1]+summaryInclude:contents.length;
-          snippet += contents.substring(start,end);
+          end = mvalue.indices[0][1]+summaryInclude<content.length?mvalue.indices[0][1]+summaryInclude:content.length;
+          snippet += content.substring(start,end);
+          
           snippetHighlights.push(mvalue.value.substring(mvalue.indices[0][0],mvalue.indices[0][1]-mvalue.indices[0][0]+1));
+          
+          
         }
       });
     }
 
     if(snippet.length<1){
-      snippet += contents.substring(0,summaryInclude*2);
+      snippet += content.substring(0,summaryInclude*2);
     }
     //pull template from hugo templarte definition
     var templateDefinition = $('#search-result-template').html();
     //replace values
     var output = render(templateDefinition,{key:key,title:value.item.title,link:value.item.uri,tags:value.item.tags,categories:value.item.categories,snippet:snippet});
-    $('#search-results').append(output);
+    //console.log(output);
+    $('#local-search-result').append(output);
 
     $.each(snippetHighlights,function(snipkey,snipvalue){
       $("#summary-"+key).mark(snipvalue);
@@ -80,18 +95,20 @@ function populateResults(result){
   });
 }
 
+
 function param(name) {
     return decodeURIComponent((location.search.split(name + '=')[1] || '').split('&')[0]).replace(/\+/g, ' ');
 }
 
 function render(templateString, data) {
+
   var conditionalMatches,conditionalPattern,copy;
   conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
   //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
   copy = templateString;
   while ((conditionalMatches = conditionalPattern.exec(templateString)) !== null) {
     if(data[conditionalMatches[1]]){
-      //valid key, remove conditionals, leave contents.
+      //valid key, remove conditionals, leave content.
       copy = copy.replace(conditionalMatches[0],conditionalMatches[2]);
     }else{
       //not valid, remove entire section
