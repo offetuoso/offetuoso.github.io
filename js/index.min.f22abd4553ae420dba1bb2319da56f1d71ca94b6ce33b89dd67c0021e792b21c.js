c18c2b7324a265642f4cde078b7fdf2b1c634b2d
offetuoso <offetuoso@gmail.com>
2021-12-24 01:35:14 +0900

    2021-12-24

---
+++ js/index.min.f22abd4553ae420dba1bb2319da56f1d71ca94b6ce33b89dd67c0021e792b21c.js
@@ -0,0 +1,4 @@
+function toggleMobileNavState(){const body=document.querySelector("body");body.classList.toggle("nav--active");}
+function initBurger(){const burger=document.querySelector(".burger");burger.addEventListener("click",toggleMobileNavState);}
+function initSubcategory(){if(window.location.pathname.indexOf('subcategories')>-1){var query=searchParam('category');var category=document.querySelector("#category__link");category.innerHTML=query[0].toUpperCase()+query.slice(1);category.href="/categories/"+query;}}
+function searchParam(key){return new URLSearchParams(location.search).get(key);};initBurger();initSubcategory();
